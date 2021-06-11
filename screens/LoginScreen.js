9ce161f57219b9bd2796925ce0f98dc893b3ba98
JI XIANG <jixiangpro@JIs-MacBook-Pro.local>
2021-06-12 00:14:06 +0800

    Added UI to login screens

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import firebase from "../database/firestoreDB";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
-export default function LoginScreen() {
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Chat App</Text>
+        <Text style={styles.fieldTitle}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
+        <Text style={styles.fieldTitle}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        ></TextInput>
+
+        <TouchableOpacity style={styles.loginButton} onPress={null}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -17,7 +49,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    paddingLeft: 30,
     justifyContent: "center",
+    paddingBottom: "50%",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 50,
+    marginBottom: 20,
+  },
+  fieldTitle: {
+    fontSize: 20,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  input: {
+    borderWidth: 1,
+    width: "90%",
+    padding: 10,
+    borderColor: "#ccc",
+    marginBottom: 20,
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    padding: 20,
+    width: 100,
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
